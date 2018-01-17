46d0a7c8c92549fdf5f3d693dc2659483d541c8f
Dino Srdoč <dsrd@hal-9001.domain_not_set.invalid>
2018-01-17 10:53:39 +0100

    initial commit

--- QuotesNotOats/ViewController.swift
+++ QuotesNotOats/ViewController.swift
@@ -8,11 +8,67 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+struct Quote: Codable, CustomStringConvertible {
+    let quoteAuthor: String
+    let quoteText: String
+    
+    var description: String {
+        return """
+            --- \(quoteAuthor) ---
+            \(quoteText)
+            """
+    }
+}
 
+class QuoteFetcher {
+    let session = URLSession.shared
+    static let urlPath = "https://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en"
+    static let url = URL(string: urlPath)
+    
+    static func fetch(_ completion: @escaping (Quote?) -> Void) {
+        guard let url = url else {
+            print("badurl")
+            completion(nil)
+            return
+        }
+        let request = URLRequest(url: url)
+        
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            if let data = data {
+                let quote = try? JSONDecoder().decode(Quote.self, from: data)
+                completion(quote)
+            }
+        }
+        
+        task.resume()
+    }
+}
+
+class ViewController: UIViewController {
+    @IBOutlet weak var authorLabel: UILabel!
+    @IBOutlet weak var quoteLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        showQuoteInUI()
+    }
+    
+    @IBAction func getQuote(_ sender: UIButton) {
+        showQuoteInUI()
+    }
+    
+    func showQuoteInUI() {
+//        DispatchQueue.global(qos: .userInteractive).async {
+            QuoteFetcher.fetch { quote in
+                if let quote = quote {
+                    DispatchQueue.main.async {
+                        self.authorLabel.text = quote.quoteAuthor.isEmpty ? "Anonymous" : quote.quoteAuthor
+                        self.quoteLabel.text = quote.quoteText
+                    }
+                }
+            }
+//        }
     }
 
     override func didReceiveMemoryWarning() {
